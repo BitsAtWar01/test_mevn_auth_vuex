3ec1692ed8ce4aa1cb11e589599ad13d832ea701
Bhavesh <bhavesh.ittadwar16@vit.edu>
2020-07-09 13:36:44 +0530

    Production Database Added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,18 @@
-module.exports = {
-    mongoURI: "mongodb://localhost:27017/mevn_auth",
+const mongoAtlasCredentials = {
+    username: 'abc123',
+    password: '123abc'
+}
+
+const dbname = 'mevn_auth';
+
+if(process.env.NODE_ENV === 'production') {
+    module.exports = {
+    mongoURI: "mongodb+srv://" + mongoAtlasCredentials.username + ":" + mongoAtlasCredentials.password + "@test-mevn-auth.d8bhq.mongodb.net/" + dbname + ">?retryWrites=true&w=majority",
     secret: "yoursecret"
+};    
+} else {
+    module.exports = {
+    mongoURI: "mongodb://localhost:27017/" + dbname,
+    secret: "yoursecret"
+};
 }
